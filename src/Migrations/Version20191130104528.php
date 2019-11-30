41486aec85d189d5d57f79137227757b6c1f1e10
C_MARTIN <m.ch55000@gmail.com>
2019-11-30 11:49:48 +0100

    CartService for reserve tickets

---
+++ src/Migrations/Version20191130104528.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191130104528 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ligne_request DROP INDEX UNIQ_A40EB1A6700047D2, ADD INDEX IDX_A40EB1A6700047D2 (ticket_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ligne_request DROP INDEX IDX_A40EB1A6700047D2, ADD UNIQUE INDEX UNIQ_A40EB1A6700047D2 (ticket_id)');
+    }
+}
